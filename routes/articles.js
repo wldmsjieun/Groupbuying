6124bd53671994cf7e9dfc350c5f08166914399c
tasddc1226 <tasddc1226@naver.com>
2019-10-25 17:14:56 +0900

    commit

--- routes/articles.js
+++ routes/articles.js
@@ -21,7 +21,8 @@ router.post('/add', function(req, res){
 
   // Get Errors
   let errors = req.validationErrors();
-
+////commit 을 위한 주석
+////////
   if(errors){
     res.render('add_article', {
       title:'Add Article',
