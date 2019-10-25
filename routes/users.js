15b69acb82c5a657da52e84abf8bf6e65b389f89
tasddc1226 <tasddc1226@naver.com>
2019-10-25 19:19:34 +0900

    first

--- routes/users.js
+++ routes/users.js
@@ -21,9 +21,9 @@ router.post('/register', function(req, res){
   const phone = req.body.phone;
   const address = req.body.address;
 
-  req.checkBody('name', '이름을 입력하세요.').notEmpty();
-  req.checkBody('email', '이메일을 입력하세요.').notEmpty();
-  req.checkBody('email', '유효하지 않은 이메일입니다.').isEmail();
+  req.checkBody('name', '이름을 입력하세요!').notEmpty();
+  req.checkBody('email', '이메일을 입력하세요!').notEmpty();
+  req.checkBody('email', '유효하지 않은 이메일입니다!').isEmail();
   req.checkBody('username', '아이디를 입력하세요.').notEmpty();
   req.checkBody('password', '비밀번호를 입력하세요.').notEmpty();
   req.checkBody('password2', '비밀번호가 일치하지 않습니다.').equals(req.body.password);
