cb4d4e0826b60c9ce3d3b983dc1d82f096968229
Jimin16 <qlrwlals1234@naver.com>
2019-10-28 00:37:03 +0900

    모든 화면에서 동일한 화면 나오도록 수정(기기에 따라 화면 틀어짐 현상 개선 ), mypage 버튼 한글화

---
+++ public/js/Queue.js
@@ -0,0 +1,32 @@
+class Queue {
+    constructor() {
+      this._arr = [];
+    }
+    enqueue(item) {
+      this._arr.push(item);
+    }
+    dequeue() {
+      return this._arr.shift();
+    }
+    length(){
+        return this._arr.length;
+    }
+    isEmpty(){
+        return this._arr.length==0?true:false;
+    }
+    isElement(data){    //요소가 큐에 있는지 확인
+        a=0;
+        for(i=0;i<this.length;i++){
+            if(this._arr[i]==data){
+                a++;
+            }
+        }
+        if(a==0){
+            return false;
+        }
+        else return true;
+    }
+}
+
+exports.Queue = Queue;
+
